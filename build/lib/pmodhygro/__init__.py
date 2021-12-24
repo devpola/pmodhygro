9861754018354cd94a54855fbe55adc32af324dc
devpola <sonamoo9771@gmail.com>
2021-12-24 17:03:08 +0900

    Delete folders about building & uploading package

--- build/lib/pmodhygro/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-    import class that defines procedure of Pmod HYGRO
-    - relative humidity sensor with an integrated temperature sensor
-"""
-from .pmodhygro import PmodHygro
-
-__all__ = ['pmodhygro']
